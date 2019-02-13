d5f77565f72e5c2a34ab199a8669c315638baefb
hoodielive <hoodielive@gmail.com>
2019-02-12 21:13:52 -0500

    added static method

--- refresher/student_class.py
+++ refresher/student_class.py
@@ -7,8 +7,14 @@ class Student:
     def average(self):
         return sum(self.marks) / len(self.marks)
     
-
+    @classmethod
+    def go_to_school(cls):
+        print("I'm going to school")
+        print("I'm a {}".format(cls))
+    
 dude = Student("dude","MIT")
 dude.marks.append(99)
 dude.marks.append(55)
+dude2 = Student('Dude', "MIT")
+dude2.go_to_school()
 print(dude.average())
