2e545e97664128e50a5e152ea9e3ae0a116416d6
hoodielive <hoodielive@gmail.com>
2019-02-14 15:10:19 -0500

    unit and integration tests

--- automated_software_testing/blog/blog.py
+++ automated_software_testing/blog/blog.py
@@ -1,3 +1,6 @@
+from post import Post
+
+
 class Blog:
     def __init__(self, title, author):
         self.title = title
@@ -5,10 +8,14 @@ class Blog:
         self.posts = []
 
     def __repr__(self):
-        pass
+        """ Returns a string representation of blog """
+        return '{} by {} ({} post{})'.format(self.title,
+                                             self.author,
+                                             len(self.posts),
+                                             's' if len(self.posts) != 1 else '')
 
     def create_post(self, title, content):
-        pass
+       self.posts.append(Post(title, content))
 
     def json(self):
         pass
