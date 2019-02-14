d92154073a815a8d871a98591f9dfc4d6db47cda
hoodielive <hoodielive@gmail.com>
2019-02-14 11:43:36 -0500

    substituted salary with *args

--- refresher/inheritance.py
+++ refresher/inheritance.py
@@ -8,18 +8,18 @@ class Student:
         return sum(self.marks) / len(self.marks)
 
     @classmethod
-    def friend(cls, origin, friend_name, *args):
-        return cls(friend_name, origin.school, *args)
+    def friend(cls, origin, friend_name, *args, **kwargs):
+        return cls(friend_name, origin.school, *args, **kwargs)
 
 
 class WorkingStudent(Student):
-    def __init__(self, name, school, salary):
+    def __init__(self, name, school, salary, job_title):
         super().__init__(name, school)
         self.salary = salary
 
 
-zhavia = WorkingStudent('Zhavia', 'Oxford', 39.00)
+zhavia = WorkingStudent('Zhavia', 'Oxford', 39.00, "Software Developer")
 print(zhavia.salary)
-friend = WorkingStudent.friend(zhavia, 'hoodie', 99.00)
+friend = WorkingStudent.friend(zhavia, 'hoodie', 99.00, job_title="Software Developer")
 print(friend.name)
 print(friend.school)
