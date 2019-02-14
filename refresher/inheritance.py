4d017a394c27e75941fcf4da948406863d919dda
hoodielive <hoodielive@gmail.com>
2019-02-14 11:16:24 -0500

    inheritance

---
+++ refresher/inheritance.py
@@ -0,0 +1,25 @@
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+
+    @classmethod
+    def friend(cls, origin, friend_name, salary):
+        return cls(friend_name, origin.school, salary)
+
+
+class WorkingStudent(Student):
+    def __init__(self, name, school, salary):
+        super().__init__(name, school)
+        self.salary = salary
+
+
+zhavia = WorkingStudent('Zhavia', 'Oxford', 39.00)
+print(zhavia.salary)
+friend = WorkingStudent.friend(zhavia, 'hoodie', 99.00)
+print(friend.name)
+print(friend.school)
--- refresher/student_class.py
+++ refresher/student_class.py
@@ -1,20 +1,27 @@
 class Student:
     def __init__(self, name, school):
-        self.name = name 
+        self.name = name
         self.school = school
         self.marks = []
-    
+
     def average(self):
         return sum(self.marks) / len(self.marks)
-    
+
     @classmethod
     def go_to_school(cls):
         print("I'm going to school")
         print("I'm a {}".format(cls))
-    
-dude = Student("dude","MIT")
-dude.marks.append(99)
-dude.marks.append(55)
-dude2 = Student('Dude', "MIT")
-dude2.go_to_school()
+
+    @staticmethod
+    def dont_go_to_school():
+        print('Dont go')
+
+
+dude = Student('dude', 'MIT')
+print(dude.marks.append(99))
+print(dude.marks.append(55))
+dude2 = Student('Dude', 'MIT')
+print(dude2.go_to_school())
 print(dude.average())
+
+print(Student.dont_go_to_school())
