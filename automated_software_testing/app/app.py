fa36b981c9a1ecc30e5babf877c3b491715803c7
hoodielive <hoodielive@gmail.com>
2019-07-30 18:16:04 -0400

    Forgot to check in whatever this is

--- automated_software_testing/app/app.py
+++ automated_software_testing/app/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/') # http://www.mysite.com/
 def home():
-    return {'message': True}
+    return jsonify({'message': True})
 
 if __name__ == '__main__':
     app.run()
